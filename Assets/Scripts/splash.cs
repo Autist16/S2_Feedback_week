48ad5a0d1231800035055520c7a03c55bc2cc178
Lindsey Mackie <1502909@abertay.ac.uk>
2017-02-21 11:52:14 +0000

    Add files via upload

---
+++ Assets/Scripts/splash.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class splash : MonoBehaviour {
+	public bool playing = false;
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 0.0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playing == true) {
+			Time.timeScale = 1;
+		} else {
+			Time.timeScale = 0;
+		}
+	}
+	public void StartGame()
+	{
+		playing = true;
+	}
+}
